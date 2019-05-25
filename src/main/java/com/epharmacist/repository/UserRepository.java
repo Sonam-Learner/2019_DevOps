46b2d16a029a1a53c4084bb199455d81f15c722d
Sonam Wangmo <43248132+Sonam-Learner@users.noreply.github.com>
2019-05-25 00:38:03 -0400

    Delete UserRepository.java

--- src/main/java/com/epharmacist/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.epharmacist.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.epharmacist.models.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
