7ad567882b39887c54a54de7329e46fa116efe66
Sonam Wangmo <43248132+Sonam-Learner@users.noreply.github.com>
2019-05-25 00:37:54 -0400

    Delete RoleRepository.java

--- src/main/java/com/epharmacist/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.epharmacist.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.epharmacist.models.Role;
-
-public interface RoleRepository extends JpaRepository<Role, String>{
-
-}
